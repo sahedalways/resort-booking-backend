058a664c2a59e1dfd781437c63ac4dbf8e12cd9a
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2025-08-31 10:30:59 +0600

    logout method not found fixed

--- app/Livewire/Backend/Components/SideBar.php
+++ app/Livewire/Backend/Components/SideBar.php
@@ -3,11 +3,20 @@
 namespace App\Livewire\Backend\Components;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class SideBar extends Component
 {
     public function render()
     {
         return view('livewire.backend.components.side-bar');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        Session::flush();
+        return redirect('/');
     }
 }
